9eda4926bf23c62f4e17aa0ac32453100dd9572a
Marc Brooks (AGeekInside) <marcwbrooks@gmail.com>
2019-09-28 07:16:36 -0700

    Adding script to spin up dev container

--- scripts/spin-dev.py
+++ scripts/spin-dev.py
@@ -1,28 +1,42 @@
+import os
+import sys
+
 import click
+import docker
 
 from environs import Env
 
 
-def read_env():
-    """Reads various configuration options from the environment."""
-
-    env = Env()
-    
-    dev_user = env("USER")
-
-    env_dict = {
-        "dev_user": dev_user,
-    }
-
-    return env_dict
-
-
 @click.command()
 @click.argument("src_dir")
-def spin_dev_container(src_dir : str):
+@click.option("-r", "--repo", default="ageekinside")
+def spin_dev_container(src_dir: str, repo: str):
     """Starts a docker container with the directory mounted as workspace."""
 
-    env_dict = read_env()
+    if not os.path.isdir(src_dir):
+        print(f"{src_dir} is a not a directory")
+        sys.exit()
+
+    abs_path_src = os.path.abspath(src_dir)
+
+    image_name = f"{repo}/pydev"
+    volume_target = f"/home/{repo}/workspace"
+
+    client = docker.from_env()
+
+    container_name = f"{os.path.basename(abs_path_src)}_dev"
+    volume_dict = {abs_path_src: {"bind": volume_target}}
+
+    print(
+        f"Preparing to spin up container:\n"
+        f"Image: {image_name}\n"
+        f"Host dir: {abs_path_src}\n"
+        f"Container name: {container_name}\n"
+        f"Container workspace: {volume_target}\n"
+    )
+    client.containers.run(
+        image_name, name=container_name, volumes=volume_dict, detach=True
+    )
 
 
 if __name__ == "__main__":
